432175590a78efe54ce23af881194550bd32295d
christiecc8 <christie.cc8@gmail.com>
2022-09-25 17:55:51 -0400

    Artist info added

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,7 +6,7 @@ const Main: React.FC<{}> = () => {
     <div className="flex flex-col p-5 items-center">
       <div className="flex flex-col -mt-16 items-center">
         <img src={`${process.env.PUBLIC_URL}/boobs-hero.png`} className="h-3/5"/>
-        <h1 className="text-3xl text-white">The Project You Never Knew You Needed</h1>
+        <h1 className="section-header">The Project You Never Knew You Needed</h1>
         <div className="flex flex-col items-center lg:grid grid-cols-3 grid-rows-1 auto-rows-max justify-center gap-10 lg:mx-20 lg:my-10 order-2 lg:order-1">
           <div className="why-1">
             <h1 className="why-header">Women in Web3 Project</h1>
@@ -34,10 +34,29 @@ const Main: React.FC<{}> = () => {
           selected NFTitties will be sold as an open edition, and
           the sale will last until October 31st, 2022.
         </h2>
-        <button className="bg-white text-2xl px-5 py-2 rounded-lg hover:bg-pink-200 order-3">Mint a NFTitty today!</button>
+        <button className="bg-white text-3xl px-5 py-2 rounded-lg hover:bg-pink-200 order-3 mt-10">Mint a NFTitty today!</button>
         </div>
         <div className="items-center m-10">
           <img src={`${process.env.PUBLIC_URL}/proceeds-split.png`}/>
+        </div>
+        <h1 className="section-header">About the Artists</h1>
+        <div className="flex flex-col lg:flex-row w-full gap-2 items-center mt-10 justify-center p-10">
+          <div className="space-y-10 items-center lg:justify-start lg:w-2/5 mb-10">
+            <img src={`${process.env.PUBLIC_URL}/bitchcoin.png`}/>
+            <h1 className="text-center text-white lg:text-left">Predating the launch of Ethereum by five months, in February 2015 conceptual artist Sarah Meyohas released the first tokenization of art on the blockchain: Bitchcoin. Today, Bitchcoin is evolving into a hub for the promotion & incubation of new ideas, deeply rooted in Web3. We are excited to be working together to develop NFTitties.</h1>
+          </div>
+          <div className="flex flex-col lg:flex-row space-y-10 lg:space-y-0 lg:justify-end lg:w-3/5">
+            <div className="artist-info">
+              <div className="artist-headshot-carlota"/>
+              <h1 className="artist-name">Carlota Dochao Naveira</h1>
+              <h2 className="artist-about">Founder, NFTitties</h2>
+            </div>
+            <div className="artist-info">
+              <div className="artist-headshot-sarah"/>
+              <h1 className="artist-name">Sarah Meyohas</h1>
+              <h2 className="artist-about">Conceptual Artist & Founder of Bitchcoin</h2>
+            </div>
+          </div>
         </div>
     </div>
   )
